a23c219a7c65686500cd1fe4687f7b2b04e4ea14
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2022-05-17 14:04:59 +0530

    updates

--- src/test/java/annotations/DependsOnGroup.java
+++ src/test/java/annotations/DependsOnGroup.java
@@ -3,10 +3,17 @@ package annotations;
 import org.testng.annotations.Test;
 
 public class DependsOnGroup {
+	
+
+	
+	@Test(priority=-5,invocationCount=5)
+	public void priority1() {
+		System.out.println(1/0);
+	}
 
 	@Test(groups= {"group1"})
 	public void group1() {
-		System.out.println(1/0);
+		System.out.println("group1");
 	}
 	
 	@Test(groups= {"group2"})
@@ -23,4 +30,6 @@ public class DependsOnGroup {
 	public void group2and3() {
 		System.out.println("I am belongs to group2and3");
 	}
+	
+    
 }
--- src/test/java/testNGParallel/RunningTwoMethodsParallel.java
+++ src/test/java/testNGParallel/RunningTwoMethodsParallel.java
@@ -6,11 +6,19 @@ public class RunningTwoMethodsParallel {
 
 	@Test
 	public  void test1() {
+		
+		for(int i=0;i<=20;i++) {
+			System.out.println("the value is:"+i);
+		}
 		System.out.println("I am test1 thread ID is = "+ Thread.currentThread().getId());
 	}
 	
 	@Test
 	public void test2() {
-		System.out.println("I am test2 thread ID is = "+ Thread.currentThread().getId());
+		for(int i=40;i<=60;i++) {
+			System.out.println("the value is:"+i);
+		}
+		System.out.println("I am "
+				+ "test2 thread ID is = "+ Thread.currentThread().getId());
 	}
 }
